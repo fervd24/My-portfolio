03c8cd726bf97a1bd9538add4991320d5d165b7f
fernando villalobos <fvd_24_93@hotmail.com>
2022-08-15 17:33:32 -0500

    Socnetworks component to show icons of the social networks and styles

---
+++ src/components/SocialNetworksIcons/SocNetIcons.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import iconsData from "./iconsData";
+
+import "./SocNetIcons.css";
+
+export default function SocNetIcons() {
+    return(
+        <div className="socNetIconsContainer">
+            {iconsData.map(icon => {
+                return (
+                    <a key={icon.id} href={icon.ref} target="_blank" rel="noreferrer">
+                        <div className={`${icon.className} iconContainer`}>
+                            <FontAwesomeIcon icon={icon.name} className="icon"/>
+                        </div>
+                    </a>
+                )
+            })}
+        </div>
+    )
+}
---
+++ src/components/SocialNetworksIcons/iconsData.js
@@ -0,0 +1,28 @@
+import { 
+    faGithub,
+    faLinkedin
+ } from "@fortawesome/free-brands-svg-icons";
+
+ import { faFileInvoice} from "@fortawesome/free-solid-svg-icons"
+const iconsData = [
+    {
+        id: 1,
+        name: faGithub,
+        className: "ghContainer",
+        ref: "https://github.com/fervd24"
+    },  
+    {
+        id: 2,
+        name: faLinkedin,
+        className: "liContainer",
+        ref: "https://www.linkedin.com/in/fernando-villalobos-davila-516a09123/"
+    },
+    {
+        id: 3,
+        name: faFileInvoice,
+        className: "cvContainer",
+        ref: "https://google.com/"
+    },
+];
+
+export default iconsData;
