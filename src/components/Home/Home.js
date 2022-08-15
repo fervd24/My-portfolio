d7502cfcff4ae0a18e5bbe48cccb3d7c29319534
fernando villalobos <fvd_24_93@hotmail.com>
2022-08-15 17:18:04 -0500

    Home component tath contain the header and body of the main page and styles

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Body from "./Body";
+
+import "./Home.css";
+
+
+export default function Home() {
+    return(
+        <div className="mainHomeContainer">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
